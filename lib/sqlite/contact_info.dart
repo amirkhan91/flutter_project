7bf3753bb2446a06aca867b243b544ebe3fa5671
Amir Khan <amir91khan@gmail.com>
2024-09-14 18:58:35 +0530

    new changes sql

--- lib/sqlite/contact_info.dart
+++ lib/sqlite/contact_info.dart
@@ -1,3 +1,83 @@
+// import 'package:sqflite/sqflite.dart';
+// import 'package:path/path.dart';
+
+// class Contact {
+//   final int? id;
+//   final String name;
+//   final String phone;
+
+//   Contact({this.id, required this.name, required this.phone});
+
+//   // Convert a Contact into a Map (for inserting in the database)
+//   Map<String, dynamic> toMap() {
+//     return {
+//       'id': id,
+//       'name': name,
+//       'phone': phone,
+//     };
+//   }
+// }
+
+// class DatabaseHelper {
+//   static final DatabaseHelper _instance = DatabaseHelper._internal();
+//   factory DatabaseHelper() => _instance;
+//   DatabaseHelper._internal();
+
+//   static Database? _database;
+
+//   Future<Database> get database async {
+//     if (_database != null) return _database!;
+//     _database = await _initDatabase();
+//     return _database!;
+//   }
+
+
+
+//   Future<Database> _initDatabase() async {
+//     String path = join(await getDatabasesPath(), 'contacts.db');
+//     return await openDatabase(
+//       path,
+//       version: 1,
+//       onCreate: (db, version) {
+//         return db.execute(
+//           "CREATE TABLE contacts(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, phone TEXT)",
+//         );
+//       },
+//     );
+//   }
+
+//   Future<void> insertContact(Contact contact) async {
+//     final db = await database;
+//     await db.insert('contacts', contact.toMap(),
+//         conflictAlgorithm: ConflictAlgorithm.replace);
+//   }
+
+//   Future<List<Contact>> getContacts() async {
+//     final db = await database;
+//     final List<Map<String, dynamic>> maps = await db.query('contacts');
+
+//     return List.generate(maps.length, (i) {
+//       return Contact(
+//         id: maps[i]['id'],
+//         name: maps[i]['name'],
+//         phone: maps[i]['phone'],
+//       );
+//     });
+//   }
+
+//   Future<void> deleteContact(int id) async {
+//     final db = await database;
+//     await db.delete('contacts', where: 'id = ?', whereArgs: [id]);
+//   }
+
+//   Future<void> updateContact(Contact contact) async{
+//     final db = await database;
+//        await db.update('contacts',contact.toMap() ,where: 'id = ?', whereArgs: [contact.id]);
+//   }
+// }
+
+//...........................
+
 import 'package:sqflite_sqlcipher/sqflite.dart';
 import 'package:path/path.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
@@ -41,7 +121,7 @@ class DatabaseHelper {
     String? password = await _secureStorage.read(key: _dbPasswordKey);
 
     if (password == null) {
-      password = 'your_secure_password';  // Replace this with  generated password
+      password = 'your_secure_password';  // Replace this with a generated password
       await _secureStorage.write(key: _dbPasswordKey, value: password);
     }
     return password;
--- lib/sqlite/contact_ui.dart
+++ lib/sqlite/contact_ui.dart
@@ -1,3 +1,155 @@
+// import 'package:flutter/material.dart';
+// import 'package:test_proj/sqlite/contact_info.dart';
+
+// class ContactListPage extends StatefulWidget {
+//   @override
+//   _ContactListPageState createState() => _ContactListPageState();
+// }
+
+// class _ContactListPageState extends State<ContactListPage> {
+//   final _nameController = TextEditingController();
+//   final _phoneController = TextEditingController();
+//   List<Contact> _contacts = [];
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     _fetchContacts();
+//   }
+
+//   Future<void> _fetchContacts() async {
+//     final contacts = await DatabaseHelper().getContacts();
+//     setState(() {
+//       _contacts = contacts;
+//     });
+//     print('Fetched contacts: ${_contacts.map((contact) => contact.name).toList()}');
+//   }
+
+//   Future<void> _addContact() async {
+//     final name = _nameController.text;
+//     final phone = _phoneController.text;
+
+//     if (name.isNotEmpty && phone.isNotEmpty) {
+//       final contact = Contact(name: name, phone: phone);
+//       print('Adding contact: Name: $name, Phone: $phone');
+
+//       await DatabaseHelper().insertContact(contact);
+//       _nameController.clear();
+//       _phoneController.clear();
+//       _fetchContacts();
+//     }
+//   }
+
+//   Future<void> _deleteContact(int id) async {
+//     print('Deleting contact with ID: $id');
+//     await DatabaseHelper().deleteContact(id);
+//     _fetchContacts();
+//   }
+
+//   Future<void> _updateContact(Contact contact) async {
+//     final name = _nameController.text;
+//     final phone = _phoneController.text;
+
+//     if (name.isNotEmpty && phone.isNotEmpty) {
+//       final updateContact = Contact(id: contact.id, name: name, phone: phone);
+//       await DatabaseHelper().updateContact(updateContact);
+//       _nameController.clear();
+//       _phoneController.clear();
+//       _fetchContacts();
+//     }
+//   }
+//     // Function to show a dialog for editing the contact
+//   void _showEditDialog(Contact contact) {
+//     // Pre-fill the controllers with the existing contact data
+//     _nameController.text = contact.name;
+//     _phoneController.text = contact.phone;
+
+//     showDialog(
+//       context: context,
+//       builder: (context) {
+//         return AlertDialog(
+//           title: const Text('Edit Contact'),
+//           content: Column(
+//             mainAxisSize: MainAxisSize.min,
+//             children: [
+//               TextField(
+//                 controller: _nameController,
+//                 decoration: const InputDecoration(labelText: 'Name'),
+//               ),
+//               TextField(
+//                 controller: _phoneController,
+//                 decoration: const InputDecoration(labelText: 'Phone'),
+//               ),
+//             ],
+//           ),
+//           actions: [
+//             ElevatedButton(
+//               onPressed: () {
+//                 _updateContact(contact); // Update the contact
+//                 Navigator.pop(context); // Close the dialog
+//               },
+//               child: const Text('Update'),
+//             ),
+//           ],
+//         );
+//       },
+//     );
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Contact List Data'),
+//       ),
+//       body: Column(
+//         children: [
+//           Padding(
+//             padding: const EdgeInsets.all(8.0),
+//             child: TextField(
+//               controller: _nameController,
+//               decoration: const InputDecoration(labelText: 'Name'),
+//             ),
+//           ),
+//           Padding(
+//             padding: const EdgeInsets.all(8.0),
+//             child: TextField(
+//               controller: _phoneController,
+//               decoration: const InputDecoration(labelText: 'Phone'),
+//             ),
+//           ),
+//           ElevatedButton(
+//             onPressed: _addContact,
+//             child: const Text('Add Contact'),
+//           ),
+//           Expanded(
+//             child: ListView.builder(
+//               itemCount: _contacts.length,
+//               itemBuilder: (context, index) {
+//                 final contact = _contacts[index];
+//                 return ListTile(
+//                     title: Text(contact.name),
+//                     subtitle: Text(contact.phone),
+//                     trailing: Row(
+//                       children: [
+//                          IconButton(
+//                     icon: const Icon(Icons.delete),
+//                     onPressed: () => _deleteContact(contact.id!),
+//                   ),
+//                    IconButton(
+//                     icon: const Icon(Icons.edit),
+//                     onPressed: () => _showEditDialog(contact),
+//                   )
+//                       ],
+//                     ));
+//               },
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
 import 'package:flutter/material.dart';
 import 'package:test_proj/sqlite/contact_info.dart';
 
