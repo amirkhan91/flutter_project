3e70d8d2c49b7e95d604f2cf90e07865c4e2d54a
Amir Khan <amir91khan@gmail.com>
2024-09-16 23:08:39 +0530

    data_list

--- lib/video_capture_data/video_capture_screen.dart
+++ lib/video_capture_data/video_capture_screen.dart
@@ -1,80 +1,80 @@
-import 'dart:io';
-import 'package:flutter/material.dart';
-import 'package:camera/camera.dart';
-import 'package:path_provider/path_provider.dart';
+// import 'dart:io';
+// import 'package:flutter/material.dart';
+// import 'package:camera/camera.dart';
+// import 'package:path_provider/path_provider.dart';
 
-class VideoCaptureScreen extends StatefulWidget {
-  @override
-  _VideoCaptureScreenState createState() => _VideoCaptureScreenState();
-}
+// class VideoCaptureScreen extends StatefulWidget {
+//   @override
+//   _VideoCaptureScreenState createState() => _VideoCaptureScreenState();
+// }
 
-class _VideoCaptureScreenState extends State<VideoCaptureScreen> {
-  CameraController? _controller;
-  late List<CameraDescription> cameras;
-  bool isRecording = false;
-  CameraLensDirection currentDirection = CameraLensDirection.back;
+// class _VideoCaptureScreenState extends State<VideoCaptureScreen> {
+//   CameraController? _controller;
+//   late List<CameraDescription> cameras;
+//   bool isRecording = false;
+//   CameraLensDirection currentDirection = CameraLensDirection.back;
   
-  @override
-  void initState() {
-    super.initState();
-    _initializeCamera();
-  }
+//   @override
+//   void initState() {
+//     super.initState();
+//     _initializeCamera();
+//   }
 
-  void _initializeCamera() async {
-    cameras = await availableCameras();
-    _controller = CameraController(
-      cameras.firstWhere(
-          (camera) => camera.lensDirection == CameraLensDirection.back),
-      ResolutionPreset.medium,
-    );
-    await _controller!.initialize();
-    setState(() {});
-  }
+//   void _initializeCamera() async {
+//     cameras = await availableCameras();
+//     _controller = CameraController(
+//       cameras.firstWhere(
+//           (camera) => camera.lensDirection == CameraLensDirection.back),
+//       ResolutionPreset.medium,
+//     );
+//     await _controller!.initialize();
+//     setState(() {});
+//   }
 
-  void _toggleRecording() async {
-    if (isRecording) {
-      final video = await _controller!.stopVideoRecording();
-      final path = await _saveVideoToDownloads(video);
-      Navigator.pop(context, File(path));
-    } else {
-      await _controller!.startVideoRecording();
-    }
-    setState(() {
-      isRecording = !isRecording;
-    });
-  }
+//   void _toggleRecording() async {
+//     if (isRecording) {
+//       final video = await _controller!.stopVideoRecording();
+//       final path = await _saveVideoToDownloads(video);
+//       Navigator.pop(context, File(path));
+//     } else {
+//       await _controller!.startVideoRecording();
+//     }
+//     setState(() {
+//       isRecording = !isRecording;
+//     });
+//   }
 
-  Future<String> _saveVideoToDownloads(XFile video) async {
-    final directory = await getExternalStorageDirectory();
-    final path = '${directory!.path}/${DateTime.now().millisecondsSinceEpoch}.mp4';
-    await video.saveTo(path);
-    return path;
-  }
+//   Future<String> _saveVideoToDownloads(XFile video) async {
+//     final directory = await getExternalStorageDirectory();
+//     final path = '${directory!.path}/${DateTime.now().millisecondsSinceEpoch}.mp4';
+//     await video.saveTo(path);
+//     return path;
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Capture Video')),
-      body: _controller != null && _controller!.value.isInitialized
-          ? Column(
-              children: [
-                AspectRatio(
-                  aspectRatio: _controller!.value.aspectRatio,
-                  child: CameraPreview(_controller!),
-                ),
-                IconButton(
-                  icon: Icon(isRecording ? Icons.stop : Icons.videocam),
-                  onPressed: _toggleRecording,
-                ),
-              ],
-            )
-          : Center(child: CircularProgressIndicator()),
-    );
-  }
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(title: Text('Capture Video')),
+//       body: _controller != null && _controller!.value.isInitialized
+//           ? Column(
+//               children: [
+//                 AspectRatio(
+//                   aspectRatio: _controller!.value.aspectRatio,
+//                   child: CameraPreview(_controller!),
+//                 ),
+//                 IconButton(
+//                   icon: Icon(isRecording ? Icons.stop : Icons.videocam),
+//                   onPressed: _toggleRecording,
+//                 ),
+//               ],
+//             )
+//           : Center(child: CircularProgressIndicator()),
+//     );
+//   }
 
-  @override
-  void dispose() {
-    _controller?.dispose();
-    super.dispose();
-  }
-}
+//   @override
+//   void dispose() {
+//     _controller?.dispose();
+//     super.dispose();
+//   }
+// }
